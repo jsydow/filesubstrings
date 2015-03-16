04e500211790cda8d42f821b35c73737a9c3ef09
Jan Sydow <jan.sydow@traxas-media.de>
2015-03-16 15:38:32 +0100

    added documentation for Program.cs

--- FileSubstrings/Program.cs
+++ FileSubstrings/Program.cs
@@ -9,8 +9,17 @@ using System.Threading.Tasks;
 
 namespace FileSubstrings
 {
+    /// <summary>
+    /// The console program that collects all substrings of all files in a directory and its subdirectories and prints those 
+    /// substring to stdout. Alternatively all file names of files that have a specific substring are printed out.
+    /// </summary>
     class Program
     {
+        /// <summary>
+        /// This class resembles a command line switch. If the command is found in the command line parameters
+        /// then a specified function is called. A switch may have an argument which is passed as the next
+        /// command line parameter. 
+        /// </summary>
         class CommandSwitch
         {
             public CommandSwitch( string command, bool hasParameter, Action<string> switchFunction)
@@ -20,11 +29,28 @@ namespace FileSubstrings
                 SwitchFunction = switchFunction;
             }
 
+            /// <summary>
+            /// The command line switch.
+            /// </summary>
             public string Command;
+
+            /// <summary>
+            /// If HasParameter is true the next command line parameter is passed to the 
+            /// SwitchFunction.
+            /// </summary>
             public bool HasParameter;
+
+            /// <summary>
+            /// The function that is called when the command is found in the command line arguments.
+            /// If HasParameter is true then the parameter to this function is the next command line argument.
+            /// Otherwise the switch itself is passed to this function.
+            /// </summary>
             public Action<string> SwitchFunction;
         }
 
+        /// <summary>
+        /// The list of all command line switches available
+        /// </summary>
         static List<CommandSwitch> _switches = new List<CommandSwitch>()
             {
                 new CommandSwitch("-sl", true, SetLength),
@@ -36,39 +62,86 @@ namespace FileSubstrings
                 new CommandSwitch("--help", false, x =>  _showHelp = true),
             };
 
+        /// <summary>
+        /// The path to the directory that is looked into
+        /// </summary>
         static string _path = Directory.GetCurrentDirectory();
+
+        /// <summary>
+        /// The minimum length (character count) of a substring
+        /// </summary>
         static int _minSubstringLength = 3;
+
+        /// <summary>
+        /// The minimum number of occurrences of a substring necessary
+        /// in order to be considered
+        /// </summary>
         static int _minOccurenceCount = 2;
+
+        /// <summary>
+        /// The maximum number of results displayed.
+        /// </summary>
         static int _maxSubstrings = Int32.MaxValue;
+
+        /// <summary>
+        /// The specific substring whos file are to be printed out.
+        /// Is null if subtrings are printed out.
+        /// </summary>
         static string _substring = null;
+
+        /// <summary>
+        /// When printing out file names: Print out full path or just file name?
+        /// </summary>
         static bool _fullPath = false;
+
+        /// <summary>
+        /// Show help?
+        /// </summary>
         static bool _showHelp = false;
+
+        /// <summary>
+        /// Has a specific substring been specified?
+        /// </summary>
         static bool _hasSubstring = false;
 
+        /// <summary>
+        /// Parses the command line arguments using the command switches.
+        /// </summary>
+        /// <param name="args">The command line arguments</param>
         static void ParseCommands(string[] args)
         {
-
+            // if non-null this is the last command. It expects an argument
             CommandSwitch currentSwitch = null;
+
+            // iterate over all arguments
             foreach (var arg in args)
             {
                 if(currentSwitch == null)
                 {
+                    // read next switch
+
                     bool hasSwitch = false;
                     foreach (var sw in _switches)
                     {
                         if(sw.Command == arg)
                         {
+                            // command found
                             if(sw.HasParameter)
                             {
+                                // command has argument, execute switching function with next parameter
                                 currentSwitch = sw;
                             }
                             else
                             {
+                                // no argument, execute switching function directly.
                                 sw.SwitchFunction(arg);
                             }
+
                             hasSwitch = true;
                         }
                     }
+
+                    // if no switch was found then this is the directory
                     if(!hasSwitch)
                     {
                         _path = arg;
@@ -76,6 +149,7 @@ namespace FileSubstrings
                 }
                 else
                 {
+                    // read argument to last switch and execute switching function.
                     currentSwitch.SwitchFunction(arg);
                     currentSwitch = null;
                 }
@@ -116,6 +190,11 @@ namespace FileSubstrings
             }
         }
 
+        /// <summary>
+        /// This is the main method that is executed when this program is started.
+        /// </summary>
+        /// <param name="args">The command line parameters</param>
+        /// <returns>The exit code. Returns 1 on errors, 0 otherwise.</returns>
         static int Main(string[] args)
         {
             ParseCommands(args);
@@ -135,6 +214,7 @@ namespace FileSubstrings
                 return 0;
             }
 
+            // initialize extractor
             FileSubstringExtractor extractor = new FileSubstringExtractor(new FileSubstringExtractor.ExtractorOptions {
                 maxSubstrings = _maxSubstrings,
                 minOccurenceCount = _minOccurenceCount,
@@ -142,21 +222,24 @@ namespace FileSubstrings
                 path = _path
             });
 
+            // do the extraction and printing of the result
             try
             {
                 if (_hasSubstring)
                 {
+                    // output files for a given substring
                     var files = extractor.getFiles(_substring);
                     if (files.Any())
                     {
                         try
                         {
+                            // write filenames.
                             files.ToList().ForEach(x =>
                                 Console.WriteLine(_fullPath ? x.FullName : x.Name)
                                 );
                         } catch( Exception)
                         {
-                            // just ignore the file
+                            // if a single file reports an error then just ignore the file
                         }
                     }
                     else
@@ -166,6 +249,7 @@ namespace FileSubstrings
                 }
                 else
                 {
+                    // write out substrings
                     foreach (var substr in extractor.getSubstrings())
                     {
                         Console.WriteLine(substr.Name + " : " + substr.OccurrenceCount);
